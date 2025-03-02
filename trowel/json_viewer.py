008e95ea7b2a0c2d3c36fcd602d826630e25c796
Ciaran De Groot <ciaran.deg@gmail.com>
2025-03-02 13:18:09 -0700

    Refactor json tree into a class

--- trowel/json_viewer.py
+++ trowel/json_viewer.py
@@ -2,29 +2,29 @@ import json
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, Tree
 
-def load_json_file(file_path: str):
-  with open(file_path, "r", encoding="utf-8") as file:
-    return json.load(file)
+class JsonTree(Tree):
+  def __init__(self, description, file_path):
+    super().__init__(description)
+    data = self._load_json_file(file_path)
+    self.root.expand()
+    self._build_tree(data, self.root)
 
-def build_json_tree(data, node):
-    """Recursively build a tree structure from a nested dictionary."""
-    if isinstance(data, dict):
-        for key, value in data.items():
-            subtree = node.add(key)
-            build_json_tree(value, subtree)
-    elif isinstance(data, list):
-        for index, item in enumerate(data):
-            subtree = node.add(f"[{index}]")
-            build_json_tree(item, subtree)
-    else:
-        node.add_leaf(str(data))
+  def _load_json_file(self, file_path: str):
+    with open(file_path, "r", encoding="utf-8") as file:
+      return json.load(file)
 
-def json_tree(file_path: str):
-    data = load_json_file(file_path)
-    tree = Tree("Plan Output")
-    tree.root.expand()
-    build_json_tree(data, tree.root)
-    return tree
+  def _build_tree(self, data, node):
+      """Recursively build a tree structure from a nested dictionary."""
+      if isinstance(data, dict):
+          for key, value in data.items():
+              subtree = node.add(key)
+              self._build_tree(value, subtree)
+      elif isinstance(data, list):
+          for index, item in enumerate(data):
+              subtree = node.add(f"[{index}]")
+              self._build_tree(item, subtree)
+      else:
+          node.add_leaf(str(data))
 
 class JsonViewerApp(App):
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
@@ -37,8 +37,7 @@ class JsonViewerApp(App):
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
-
-        yield json_tree(self.json_file_path)
+        yield JsonTree("Plan Output", self.json_file_path)
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""

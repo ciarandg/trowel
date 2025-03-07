c9ae18883877c158500fc0216e8cc69f36f0e9da
Ciaran De Groot <ciaran.deg@gmail.com>
2025-03-06 20:12:36 -0700

    Add other CRUD operations

--- trowel/tf_plan_viewer.py
+++ trowel/tf_plan_viewer.py
@@ -2,6 +2,89 @@ import json
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, Tree
 
+class Parser():
+    def __init__(self, json_data):
+        self.json_data = json_data
+
+    def _all_field_names(self, resource):
+        keys = ["before", "after", "after_unknown", "before_sensitive", "after_sensitive"];
+        entries = [resource["change"].get(k) for k in keys]
+        filtered = [e for e in entries if e != False and e != None]
+        field_name_lists = [list(e.keys()) for e in filtered]
+        return sorted(list(set([x for xs in field_name_lists for x in xs])))
+
+    def _get_before_after(self, resource, field_name):
+        out = []
+        change_dict = resource["change"]
+        before = change_dict.get("before")
+        before_val = before.get(field_name) if before else None # before is None for create operations
+        before_sensitive = change_dict.get("before_sensitive")
+        before_sensitive_val = before_sensitive.get(field_name) if before_sensitive else None # before_sensitive is None for create operations
+        before_text = "Before: "
+        if not (before_val is None):
+            before_text += json.dumps(before_val)
+        elif not (before_sensitive_val is None):
+            before_text += f"{json.dumps(before_sensitive_val)} (sensitive)"
+        else:
+            before_text += json.dumps(None)
+
+        after = change_dict.get("after")
+        after_val = after.get(field_name) if after else None # after is None for destroy operations
+        after_sensitive = change_dict.get("after_sensitive")
+        after_sensitive_val = after_sensitive.get(field_name) if after_sensitive else None # after_sensitive is None for destroy operations
+        after_unknown_val = change_dict["after_unknown"].get(field_name)
+        after_text = "After: "
+        if not after_val is None:
+            after_text += json.dumps(after_val)
+        elif not after_sensitive_val is None:
+            after_text += json.dumps(after_sensitive_val)
+        elif not after_unknown_val is None:
+            after_text += "(known after apply)"
+        else:
+            after_text += json.dumps(None)
+
+        out.append(before_text)
+        out.append(after_text)
+        return out
+
+    def parse(self):
+      """Convert raw TF plan JSON to a diff structure"""
+      out = {}
+      added = out.setdefault("[bold green]Added[/bold green]", {})
+      modified = out.setdefault("[bold yellow]Modified[/bold yellow]", {})
+      replaced = out.setdefault("[bold red]Replaced[/bold red]", {})
+      removed = out.setdefault("[bold red]Removed[/bold red]", {})
+
+      for resource in self.json_data["resource_changes"]:
+          change_dict = resource["change"]
+          actions = change_dict["actions"]
+          action_reason = "action_reason" in resource and resource["action_reason"]
+          if actions == ["no-op"]:
+            continue # no changes to make for this resource
+          elif actions == ["create"]:
+            resource_addr = added.setdefault(resource["address"], {})
+            field_names = self._all_field_names(resource)
+            for f in field_names:
+              resource_addr[f] = self._get_before_after(resource, f)
+          elif actions == ["update"]:
+            resource_addr = modified.setdefault(resource["address"], {})
+            field_names = self._all_field_names(resource)
+            for f in field_names:
+              resource_addr[f] = self._get_before_after(resource, f)
+          elif actions == ["delete"]:
+            resource_addr = removed.setdefault(resource["address"], {})
+            field_names = self._all_field_names(resource)
+            for f in field_names:
+              resource_addr[f] = self._get_before_after(resource, f)
+          elif actions == ["delete", "create"]:
+            resource_addr = replaced.setdefault(resource["address"], {})
+            field_names = self._all_field_names(resource)
+            for f in field_names:
+              resource_addr[f] = self._get_before_after(resource, f)
+          else:
+              raise Exception(f"Invalid resource actions array:", actions)
+      return out
+
 class TfPlanTree(Tree):
   BINDINGS = [
     ("j", "cursor_down", "Cursor Down"),
@@ -17,66 +100,12 @@ class TfPlanTree(Tree):
   def __init__(self, description, json_file_path):
     super().__init__(description)
     json_data = self._load_json_file(json_file_path)
-    processed = self._process_data(json_data)
-    self._build_tree(processed, self.root)
+    parser = Parser(json_data)
+    self._build_tree(parser.parse(), self.root)
 
   def _load_json_file(self, file_path: str):
     with open(file_path, "r", encoding="utf-8") as file:
       return json.load(file)
-
-  def _process_data(self, json_data):
-      """Convert raw TF plan JSON to a diff structure"""
-      out = {}
-      added = out.setdefault("[bold green]Added[/bold green]", {})
-      modified = out.setdefault("[bold yellow]Modified[/bold yellow]", {})
-      replaced = out.setdefault("[bold red]Replaced[/bold red]", {})
-      removed = out.setdefault("[bold red]Removed[/bold red]", {})
-
-      for resource in json_data["resource_changes"]:
-          change_dict = resource["change"]
-          actions = change_dict["actions"]
-          action_reason = "action_reason" in resource and resource["action_reason"]
-          if len(actions) == 1 and actions[0] == "no-op":
-            continue # no changes to make for this resource
-          if action_reason == "replace_because_cannot_update":
-            resource = replaced.setdefault(resource["address"], {})
-
-            # get all field names
-            field_dict_keys = ["before", "after", "after_unknown", "before_sensitive", "after_sensitive"];
-            field_name_lists = [list(change_dict[k].keys()) for k in field_dict_keys]
-            field_names = sorted(list(set([x for xs in field_name_lists for x in xs])))
-
-            changes = {f: {} for f in field_names}
-
-            for f, change in changes.items():
-              section = resource.setdefault(f, [])
-
-              before_val = change_dict["before"].get(f)
-              before_sensitive_val = change_dict["before_sensitive"].get(f)
-              before_text = "Before: "
-              if not (before_val is None):
-                  before_text += json.dumps(before_val)
-              elif not (before_sensitive_val is None):
-                  before_text += f"{json.dumps(before_sensitive_val)} (sensitive)"
-              else:
-                  before_text += json.dumps(None)
-
-              after_val = change_dict["after"].get(f)
-              after_sensitive_val = change_dict["after_sensitive"].get(f)
-              after_unknown_val = change_dict["after_unknown"].get(f)
-              after_text = "After: "
-              if not after_val is None:
-                  after_text += json.dumps(after_val)
-              elif not after_sensitive_val is None:
-                  after_text += json.dumps(after_sensitive_val)
-              elif not after_unknown_val is None:
-                  after_text += "(known after apply)"
-              else:
-                  after_text += json.dumps(None)
-
-              section.append(before_text)
-              section.append(after_text)
-      return out
 
   def _build_tree(self, data, node):
       """Recursively build a tree structure from a nested dictionary."""

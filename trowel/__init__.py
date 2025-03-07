f292f690cceadde8c4471397e571baa0cdf357d4
Ciaran De Groot <ciaran.deg@gmail.com>
2025-03-07 16:39:19 -0700

    Add click CLI

--- trowel/__init__.py
+++ trowel/__init__.py
@@ -1,8 +1,8 @@
-import sys
+import click
 from .tf_plan_viewer import TfPlanViewerApp
 
-
-def run():
-    json_file_path = sys.argv[1]
+@click.command()
+@click.argument("json_file_path")
+def run(json_file_path):
     app = TfPlanViewerApp(json_file_path)
     app.run()

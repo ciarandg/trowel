78999b66053b3a387e3a85ffe8896380b160477f
Ciaran De Groot <ciaran.deg@gmail.com>
2025-04-06 11:28:00 -0600

    Support plan file generation

--- trowel-rs/src/main.rs
+++ trowel-rs/src/main.rs
@@ -1,10 +1,11 @@
-use std::{error::Error, ffi::OsStr, fs, io, path::PathBuf, process::Command};
+use std::{error::Error, ffi::OsStr, fs, io, path::PathBuf, process::{Command, Stdio}};
 
 use clap::{command, Parser};
 use model::trowel_diff::TrowelDiff;
 use ratatui::{
     backend::Backend, crossterm::event::{self, Event, KeyCode, KeyEventKind, KeyModifiers, MouseEventKind}, layout::Position, Terminal
 };
+use tempfile::NamedTempFile;
 
 mod app;
 mod ui;
@@ -20,7 +21,7 @@ use crate::{
 #[command(version, about = "A TUI for working with OpenTofu and Terraform", long_about = None)]
 struct Args {
     #[arg(short, long)]
-    plan_file: PathBuf,
+    plan_file: Option<PathBuf>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -29,7 +30,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     let plan_file = args.plan_file;
-    let diff = generate_diff(plan_file)?;
+    let diff = match plan_file {
+        Some(p) => generate_diff_from_plan(p),
+        None => generate_diff()
+    }?;
 
     let mut terminal = ratatui::init();
     let mut app = App::new(diff);
@@ -39,11 +43,40 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn generate_diff(plan_file: PathBuf) -> Result<TrowelDiff, io::Error> {
-    let extension = plan_file.as_path().extension().and_then(OsStr::to_str);
+fn generate_binary_plan() -> Result<NamedTempFile, io::Error> {
+    let file = NamedTempFile::new()?;
+    match file.path().to_str() {
+        Some(p) => {
+            let mut cmd = Command::new("tofu")
+                .arg("plan")
+                .arg("-out")
+                .arg(p)
+                .stderr(Stdio::inherit())
+                .spawn()?;
+            let _ = cmd.wait()?;
+            Ok(file)
+        },
+        None => {
+            Err(io::Error::new(io::ErrorKind::NotFound, "No string representation available for tempfile path"))
+        }
+    }
+}
+
+fn generate_diff() -> Result<TrowelDiff, io::Error> {
+    // NOTE: NamedTempFile automatically deletes its tempfile when dropped via its destructor, and so should be dropped explicitly
+    let binary_tempfile = generate_binary_plan()?;
+    let diff = generate_diff_binary(binary_tempfile.path().to_path_buf());
+    drop(binary_tempfile);
+    diff
+}
+
+fn generate_diff_from_plan(plan_file: PathBuf) -> Result<TrowelDiff, io::Error> {
+    let path = plan_file.as_path();
+    let extension = path.extension().and_then(OsStr::to_str);
+    let json_data = fs::read_to_string(&plan_file)?;
 
     match extension {
-        Some("json") => generate_diff_json(fs::read_to_string(plan_file)?),
+        Some("json") => generate_diff_json(json_data),
         _ => generate_diff_binary(plan_file), // Assume binary plan if file extension is not '.json'
     }
 }

fd04dddef5a346eb231d66d660eadd2bde1b1296
Ciaran De Groot <ciaran.deg@gmail.com>
2025-04-19 22:42:10 -0600

    Configure code coverage

--- flake.nix
+++ flake.nix
@@ -3,12 +3,16 @@
 
   inputs.crane.url = "github:ipetkov/crane";
 
+  inputs.fenix.url = "github:nix-community/fenix";
+  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
+
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
   outputs = {
     self,
     nixpkgs,
     crane,
+    fenix,
     flake-parts,
     ...
   } @ inputs: flake-parts.lib.mkFlake {inherit inputs;} {
@@ -18,13 +22,44 @@
     ];
 
     perSystem = { config, pkgs, lib, system, ... }: let
+      toolchain = fenix.packages.${system}.complete.withComponents [
+        "cargo"
+        "llvm-tools"
+        "rustc"
+      ];
       craneLib = crane.mkLib pkgs;
+      craneLibLLvmTools = craneLib.overrideToolchain toolchain;
+
+      src = craneLib.cleanCargoSource ./.;
     in {
+      apps = {
+        cargo = {
+          type = "app";
+          program = lib.getExe (pkgs.writeShellScriptBin "cargo" ''
+            ${toolchain}/bin/cargo $@
+          '');
+        };
+        coverage = {
+          type = "app";
+          program = lib.getExe (pkgs.writeShellScriptBin "coverage" ''
+            ${toolchain}/bin/cargo llvm-cov --open
+          '');
+        };
+      };
+
       packages = {
         trowel = craneLib.buildPackage {
-          src = craneLib.cleanCargoSource ./.;
+          inherit src;
         };
         default = self.packages.${system}.trowel;
+
+        # https://crane.dev/API.html#cranelibcargollvmcov
+        lcov = craneLibLLvmTools.cargoLlvmCov {
+          inherit src;
+          cargoArtifacts = craneLib.buildDepsOnly {
+            inherit src;
+          };
+        };
       };
     };
   };
